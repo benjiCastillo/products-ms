287919a840bbd2d027154b4c2d34812ff833daf7
BenjiCastillo <castilloeguezbenji@gmail.com>
2025-09-04 11:37:46 -0400

    cambio a nats

--- config/envs.ts
+++ config/envs.ts
@@ -4,16 +4,21 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
   .object<EnvVars>({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const envVarsResult = envVarsSchema.validate(process.env);
+const envVarsResult = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(',') || [],
+});
 
 if (envVarsResult.error) {
   throw new Error(
@@ -28,4 +33,5 @@ console.log(envVars.PORT);
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
+  natsServers: envVars.NATS_SERVERS,
 };
